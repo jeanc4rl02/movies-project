e38fdd51e1550357fcac15c9345fe954a9e16ccd
Marin28c1997 <96443496+Marin28c1997@users.noreply.github.com>
2023-04-19 14:51:00 -0500

    nav

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,20 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <Nav/>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
+
+<script>
+
+import Nav from './components/Nav.vue';
+
+export default {
+  components: {
+    Nav,
+  },
+};
+</script>
+
 
 <style scoped>
 .logo {
