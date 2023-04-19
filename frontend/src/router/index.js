bcf95c3312183c3ac97c85fda1ab984e2dfddffa
Marin28c1997 <96443496+Marin28c1997@users.noreply.github.com>
2023-04-19 13:10:00 -0500

    Update index.js

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,1 +1,15 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createMemoryHistory, createRouter, createWebHistory } from "vue-router";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  // history: createMemoryHistory(),
+  routes: [
+    {
+        path: "/",
+        name: "welcome",
+        component: () => import("../App.vue"),
+      },
+  ],
+});
+
+export default router;
