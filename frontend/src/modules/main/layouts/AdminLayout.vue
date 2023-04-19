5532e9723680a716c05c2c4b73fde869904d4916
andersson980114 <anderson.980114@gmail.com>
2023-04-19 15:19:34 -0500

    inicio

---
+++ frontend/src/modules/main/layouts/AdminLayout.vue
@@ -0,0 +1,18 @@
+<template>
+    <!-- ComponentAdmin -->
+    <NavAdmin />
+    <router-view></router-view>
+  </template>
+  
+  <script>
+  import NavAdmin from "../components/NavAdmin.vue";
+  
+  export default {
+    components: {
+      NavAdmin,
+    },
+  };
+  </script>
+  
+  <style></style>
+  
