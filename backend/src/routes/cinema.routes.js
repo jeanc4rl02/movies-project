24b32ddef00f1a45359be39753beb9e989e16bef
Jean <jean.carrillo@udea.edu.co>
2023-04-20 17:55:42 -0500

    Caching added with apicache

--- backend/src/routes/cinema.routes.js
+++ backend/src/routes/cinema.routes.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { getAllCinemas, getOneCinema, createCinema, deleteCinema, updateCinema } from "../controllers/cinema.controller.js";
 import fileUpload from 'express-fileupload';
+import apicache from 'apicache';
 
 const cineRouter = Router ();
+const cache = apicache.middleware
 
-cineRouter.get('/', getAllCinemas);
+cineRouter.get('/', cache('1 minutes'), getAllCinemas);
 
-cineRouter.get('/:id', getOneCinema);
+cineRouter.get('/:id', cache('2 minutes'), getOneCinema);
 
 cineRouter.post('/', fileUpload({useTempFiles : true, tempFileDir : './uploads'}), createCinema);
 
