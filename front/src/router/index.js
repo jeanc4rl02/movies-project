3ba0708bae6c5a6e7a1b1ef3075fa6dc3f746e11
jaserleney <yaser.keny@gmail.com>
2023-04-20 12:27:46 -0500

    Routes basic configured

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -8,6 +8,25 @@ const router = createRouter({
       name: "home",
       component: () => import("../modules/landingpage /views/HomeView.vue"),
     },
+    {
+      path: "/payments",
+      name: "payments",
+      component: () => import("../modules/payment/views/PaymentView.vue"),
+    },
+    {
+      path: "/cinema",
+      name: "cinema",
+      component: () => import("../modules/cinema/views/CinemaView.vue"),
+    },
+    {
+      path: "/admin",
+      name: "admin",
+      component: () => import("../modules/admin/views/AdminView.vue"),
+    },
+    {
+      path: "/:pathMatch(.*)*",
+      redirect: { name: "home" },
+    },
   ],
 });
 
